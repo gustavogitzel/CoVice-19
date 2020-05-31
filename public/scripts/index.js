adeb4d62adaacb0a061271d664c237134fb93332
Gustavo Gitzel <42344174+gitzel@users.noreply.github.com>
2020-05-31 08:28:00 -0300

    Update index.js

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -3,7 +3,10 @@ $(document).ready(function () {
     $("#sidenavRight").sidenav({
         edge: "right"
     });
+    var array_of_dom_elements = document.querySelectorAll("input[type=range]");
+    M.Range.init(array_of_dom_elements);
     $("select").formSelect();
+    
 });
 
 function RequestService() {
