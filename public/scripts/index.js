5728ae1630f356f7204e327fbcaf00855fbf980c
Gustavo Gitzel <42344174+gitzel@users.noreply.github.com>
2020-05-30 22:15:03 -0300

    website
    - minimal design

---
+++ public/scripts/index.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    $(".sidenav").sidenav();
+    $("#sidenavRight").sidenav({ edge: "right" });
+    $("select").formSelect();
+  });
+  
+  function RequestService() {
+    $.ajax({
+      type: "GET",
+      url: "url/adho/ajaj",
+      contentType: "application/json; charset=utf-8",
+      data: "{}",
+      dataType: "json",
+      success: function(data) {
+        SucessCallback(data.d);
+      },
+      error: function(data) {
+        FailureCallBack(data);
+      }
+    });
+  }
+  
+  function SucessCallback(result) {
+    $("p").html(
+      "Resultado: " + result.Message + " <br /> Descrição: " + result.Description
+    );
+  }
+  
+  function FailureCallBack(result) {
+    alert(result.status + " " + result.statusText);
+  }
+  
