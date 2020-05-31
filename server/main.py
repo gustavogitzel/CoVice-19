99f2935b6ecc913e16ceb185bedd2d5dcbf2dc4a
Scherer <fsvicentin@hotmail.com>
2020-05-31 14:47:33 -0300

    request to country data

--- server/main.py
+++ server/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, redirect, url_for, render_template, flash, json, jsonify, request
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -16,6 +17,12 @@ def submit():
     # chamar algoritmo pedro
     return "teste submit" # retorna json pro site 
     
+@app.route('/country/<name>', methods=['GET'])
+def country(name):
+	df = pd.read_csv('../final.csv')
+	df.set_index('Name', inplace=True)
+	df.drop(df.columns[0], axis=1, inplace=True)
+	return df.loc[name].to_json()
 
 if __name__ == "__main__":
     app.run()
