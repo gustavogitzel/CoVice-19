840dda85fcd9ea961521a3a3be3986ba0790375c
Scherer <fsvicentin@hotmail.com>
2020-05-31 22:55:36 -0300

    application.py updated

--- server/application.py
+++ server/application.py
@@ -1,20 +1,25 @@
-from flask import Flask, redirect, url_for, render_template, flash, json, jsonify, request
+import os
+from flask import Flask, jsonify, request, render_template, Response, send_file, stream_with_context, make_response
+import json
+import numpy as np
 import pandas as pd
 from flask_cors import CORS, cross_origin
 
-application = Flask(__name__)
-cors = CORS(application, resources={r"/api/*": {"origins": "*"}})
-application.config['CORS_HEADERS'] = 'Content-Type'
+app = Flask(__name__)
+
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}}, support_credentials=True)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 from sklearn import preprocessing
 from sklearn.linear_model import LinearRegression
 
-#Controll vars
+#Vars de controle
 path = 'final.csv'
 drop = [0, 1, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 22, 23, 24, 25, 26, 27, 28, 30, 31]
-prever = [0, 1, -1] #Indexes from de new DF!!!
+drop = [0, 4, 5, 6, 7, 8]
+prever = [0, 1, -1] #Indices do novo DF!!
 
-#Global vars
+#Vars globais
 normalizer = -1
 df = pd.DataFrame()
 lm = -1
@@ -27,10 +32,10 @@ def inialize():
     global drop
     global lm
 
-    #Reading file
+    #Lendo
     df = pd.read_csv(path)
 
-    #Correcting the columns
+    #Arrumando colunas
     df.drop(df.columns[drop], axis=1, inplace=True)
 
     df_cases = df.iloc[:, 2]
@@ -38,14 +43,14 @@ def inialize():
 
     cs = df.columns
 
-    #Normalizing the data
+    #Normalizando
     normalizer = preprocessing.Normalizer().fit(df)
     df = normalizer.transform(df)
     df = pd.DataFrame(df, columns = cs)
 
     df['Cases_per_mil'] = df_cases
 
-    #Training
+    #Treinando
     lm = LinearRegression()
     lm.fit(df.iloc[2:, 2:-1], df.iloc[2:, 1])
 
@@ -92,29 +97,25 @@ def classify(dd, hb, pa, up):
 inialize()
 
 
-@application.route("/")
-def home():
-    return "Teste"
-
-
-@application.route("/submit", methods=['POST'])
+@app.route("/submit", methods=['GET'])
 @cross_origin()
 def submit():
-    density = request.form['density']
-    icu = request.form['icu']
-    elder = request.form['elder']
-    population = request.form['population']
+    density = request.args.get('density')
+    icu = request.args.get('icu')
+    elder = request.args.get('elder')
+    population = request.args.get('population')
 
-    return classify(density, icu, elder, population).to_json() # returns json to the website 
+    return jsonify(classify(density, icu, elder, population))
     
-@application.route('/country/<name>', methods=['GET'])
+@app.route('/country/<name>', methods=['GET'])
 @cross_origin()
 def country(name):
-	df = pd.read_csv('final.csv')
-	df.set_index('Name', inplace=True)
-	df.drop(df.columns[0], axis=1, inplace=True)
-	return df.loc[name].to_json()
+    df = pd.read_csv('final.csv')
+    df.set_index('Name', inplace=True)
+    df.drop(df.columns[0], axis=1, inplace=True)
+    return df.loc[name].to_json()
+
 
 if __name__ == "__main__":
-    application.run()
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, threaded=True)
