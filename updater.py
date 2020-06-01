193e5fe6aed12a7e62ae24d200167fdd8a793156
Scherer <fsvicentin@hotmail.com>
2020-05-31 22:54:41 -0300

    updater updated

--- updater.py
+++ updater.py
@@ -12,7 +12,7 @@ urlCountries = 'https://restcountries.eu/rest/v2/all'
 urlCovidData = 'https://covid.ourworldindata.org/data/owid-covid-data.json'
 super_file = 'super_file.csv'
 #urlTemp = 'https://crudata.uea.ac.uk/cru/data/hrg/cru_ts_3.23/crucy.1506241137.v3.23/countries/tmp/crucy.v3.23.1901.2014.{}.tmp.per'
-wldMean = 795
+wldMean = 773
 
 
 df_cc = pd.read_csv(urlConfirmed)
@@ -28,10 +28,15 @@ count = 0
 for i in range(0, len(df_cc.index)-1):
     n = df_cc.iloc[i,0]
     if not n == name:
-        dict_cc["Name"].append(name)
-        dict_cc["Cases"].append(count)
-        name = df_cc.iloc[i,0]
-        count = 0
+    	if name in dict_cc["Name"]:
+    		dict_cc["Cases"][dict_cc["Name"].index(name)] += count
+    	else:
+	    	dict_cc["Name"].append(name)
+	    	dict_cc["Cases"].append(count)
+    	name = df_cc.iloc[i, 0]
+    	if name == 'US':
+    		name = 'United States of America'
+    	count = 0
     count += df_cc.iloc[i, 1]
 
 df_cc = pd.DataFrame.from_dict(dict_cc)
@@ -51,10 +56,15 @@ count = 0
 for i in range(0, len(df_de.index)-1):
     n = df_de.iloc[i,0]
     if not n == name:
-        dict_de["Name"].append(name)
-        dict_de["Deaths"].append(count)
-        name = df_de.iloc[i,0]
-        count = 0
+    	if name in df_de["Name"]:
+    		dict_de["Deaths"][dict_de["Name"].index(name)] += count
+    	else:
+	    	dict_de["Name"].append(name)
+	    	dict_de["Deaths"].append(count)
+    	name = df_de.iloc[i,0]
+    	if name == 'US':
+    		name = 'United States of America'
+    	count = 0
     count += df_de.iloc[i, 1]
 
 df_de = pd.DataFrame.from_dict(dict_de)
@@ -136,8 +146,9 @@ for l in range(0, len(df_super.index)):
         break
 
 df_super = pd.DataFrame.from_dict(dict_super)
-
-df_super.drop(['Investment in water and sanitation with private participation (current US$)', 'Community health workers (per 1,000 people)', 'Adequacy of social safety net programs (% of total welfare of beneficiary households)', 'Adequacy of social insurance programs (% of total welfare of beneficiary households)', 'Railways, passengers carried (million passenger-km)', 'Completeness of death registration with cause-of-death information (%)', 'Population in urban agglomerations of more than 1 million (% of total population)', 'Electric power consumption (kWh per capita)', 'Smoking prevalence, total (ages 15+)' ,'Average precipitation in depth (mm per year)'], axis=1, inplace=True)
+arr = ['Population ages 65 and above (% of total population)', 'Urban population (% of total population)', 'Demographic Density', 'Hospital beds (per 1,000 people)', 'Code']
+df_super.drop(df_super.loc[:, ~df_super.columns.isin(arr)], axis=1, inplace=True)
+#df_super.drop(['Investment in water and sanitation with private participation (current US$)', 'Community health workers (per 1,000 people)', 'Adequacy of social safety net programs (% of total welfare of beneficiary households)', 'Adequacy of social insurance programs (% of total welfare of beneficiary households)', 'Railways, passengers carried (million passenger-km)', 'Completeness of death registration with cause-of-death information (%)', 'Population in urban agglomerations of more than 1 million (% of total population)', 'Electric power consumption (kWh per capita)', 'Smoking prevalence, total (ages 15+)' ,'Average precipitation in depth (mm per year)'], axis=1, inplace=True)
 df_super.drop([len(df_super.index)-1], inplace=True)
 
 
@@ -166,6 +177,9 @@ df_final['Deaths'] = df_de['Deaths']
 df_final['Cases_per_mil'] = df_cpm['Cases_per_mil']
 df_final = df_final.join(df_co.set_index('Name'), on='Name')
 df_final = df_final.join(df_super.set_index('Code'), on='Code')
+df_final.set_index('Name', inplace=True)
+
+
 df_final = df_final.dropna()
 
 df_final.to_csv(path+'final.csv')
